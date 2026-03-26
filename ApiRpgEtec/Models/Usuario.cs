15b4a563855ca1f2ba795621197976170a7499d6
Jolimenezes <jolimenezes@gmail.com>
2026-03-25 21:25:02 -0300

    Add project files.

---
+++ ApiRpgEtec/Models/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApiRpgEtec.Models
+{
+    internal class Usuario
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string PasswordString { get; set; } = string.Empty;
+        public string? Perfil { get; set; }
+        public string Token { get; set; } = string.Empty;
+        public string? Email { get; set; } = string.Empty;
+        public byte[]? Foto { get; set; }
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
+    }
+}
