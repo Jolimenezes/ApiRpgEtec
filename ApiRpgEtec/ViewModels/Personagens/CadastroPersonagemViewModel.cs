4b0d9e6f77fa04b0d8bb4b26865bdd910abdde2a
Jolimenezes <jolimenezes@gmail.com>
2026-04-15 22:24:08 -0300

    Início de cadastro personagem

---
+++ ApiRpgEtec/ViewModels/Personagens/CadastroPersonagemViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ApiRpgEtec.Services.Personagens;
+
+namespace ApiRpgEtec.ViewModels.Personagens
+{
+    public class CadastroPersonagemViewModel : BaseViewModel
+    {
+        private PersonagemService pService;
+
+        public CadastroPersonagemViewModel()
+        {
+            string token = Preferences.Get("UsuarioToken", string.Empty);
+            pService = new PersonagemService(token);
+        }
+    }
+}
